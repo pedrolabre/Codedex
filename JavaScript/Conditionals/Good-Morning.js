81d7c593af44a59bc7391e0824d34d428d7c1b3c
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-08-14 16:25:17 -0300

    JavaScript - 3. Conditionals: Good Morning exercise

---
+++ JavaScript/Conditionals/Good-Morning.js
@@ -0,0 +1,7 @@
+let hour = 11;
+
+if (hour < 12) { 
+  console.log("Good morning 🌞");
+  console.log("Wake up! it's time to get out of bed");
+  console.log("you're late to work! skip your breakfast");
+}
