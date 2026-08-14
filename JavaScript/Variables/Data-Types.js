9c8d8daf8826c58a7a4a8d4523c68938d5079525
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-08-14 14:40:17 -0300

    JavaScript - 2. Variables: Data Types exercise

---
+++ JavaScript/Variables/Data-Types.js
@@ -0,0 +1,9 @@
+let companyName = "Lojas Bandeira";
+let foundingYear = 1997;
+let isActive = true;
+let fundingAmount;
+
+console.log(companyName);
+console.log(foundingYear);
+console.log(isActive);
+console.log(fundingAmount);
