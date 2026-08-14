be56ef5d39d94522683d270f5b1f8b223509728f
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-08-14 14:40:19 -0300

    JavaScript - 2. Variables: Planet Weight exercise

---
+++ JavaScript/Variables/Planet-Weight.js
@@ -0,0 +1,5 @@
+let earthWeight = 210;
+let marsWeight = earthWeight * 0.38;
+
+console.log("Your weight on Earth is " + earthWeight + " pounds.");
+console.log("Your weight on Mars is " + marsWeight + " pounds.");
