554aac54dcfb958443813e004de29b4ac77ba713
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-08-14 14:40:17 -0300

    JavaScript - 2. Variables: Let & Const exercise

---
+++ JavaScript/Variables/Let-n-Const.js
@@ -0,0 +1,10 @@
+const userName = "Pedro Labre";
+const FavoriteColor = "Blue";
+
+let CurrentLocation = "Home";
+let mood = "Inspired"
+
+console.log(userName);
+console.log(FavoriteColor);
+console.log(CurrentLocation);
+console.log(mood);
