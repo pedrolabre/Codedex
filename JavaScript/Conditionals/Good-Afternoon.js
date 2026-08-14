390b73912bacb2fbff043ecd16a7eab8465bd6a0
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-08-14 16:25:17 -0300

    JavaScript - 3. Conditionals: Good Afternoon exercise

---
+++ JavaScript/Conditionals/Good-Afternoon.js
@@ -0,0 +1,11 @@
+let hour = 16;
+
+if (hour < 12) { 
+  console.log("Good morning 🌞");
+  console.log("Wake up! it's time to get out of bed");
+  console.log("you're late to work! skip your breakfast");
+} else {
+  console.log("Good afternoon ☁️");
+  console.log("it's too late, go back to sleep");
+  console.log("i'll comeback tomorrow");
+}
