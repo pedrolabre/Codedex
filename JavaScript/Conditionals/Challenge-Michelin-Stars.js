edd7fd92b978dbd33c9998e7a96bf37a9d991b8f
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-08-22 15:46:35 -0300

    JavaScript - 3. Conditionals: Challenge - Michelin Stars exercise

---
+++ JavaScript/Conditionals/Challenge-Michelin-Stars.js
@@ -0,0 +1,11 @@
+let rating = 3
+
+if (rating == 1) {
+  console.log("🌟 is worth walking to.");
+} else if (rating == 2) {
+  console.log("🌟🌟 is worth driving to.");
+} else if (rating == 3) {
+  console.log("🌟🌟🌟 is worth flying to.");
+} else {
+  console.log("Invalid");
+}
