57cc66d78270b569e9a59be74bda4ea5eda8a83e
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-09-06 16:45:00 -0300

    JavaScript - 5. Arrays: Where's Waldo exercise

---
+++ JavaScript/Arrays/Where-s-Waldo.js
@@ -0,0 +1,19 @@
+const characters = [
+  "The Wally Watchers",
+  "Wilma",
+  "Fritz",
+  "Wizard Whitebeard",
+  "Odlaw",
+  "Waldo",
+  "Woof"
+];
+
+if (characters.includes("Waldo")) {
+
+  let waldoIndex = characters.indexOf("Waldo");
+
+  console.log("Found Waldo at index " + waldoIndex + "!");
+
+} else {
+  console.log("Not found");
+}
