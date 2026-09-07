0871c10e57370acd5edcc39ebfd76f14dd62908d
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-09-07 15:21:00 -0300

    Intermediate JavaScript - 1. ECMAScript: Arrow Logic exercise

---
+++ Intermediate-JavaScript/ECMAScript/Arrow-Logic.js
@@ -0,0 +1,13 @@
+// Rewrite as an arrow function 💖
+
+//function forceEquation(mass, acceleration) {
+//  return mass * acceleration;
+//}
+
+//console.log(forceEquation(3, 4));
+
+const forceEquation = (mass, acceleration) => {
+  return mass * acceleration;
+};
+
+console.log(forceEquation(3, 4));
