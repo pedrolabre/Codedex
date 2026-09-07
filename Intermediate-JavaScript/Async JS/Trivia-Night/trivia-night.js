db0ebe04b981a4d251b5a23d0ebee4d469136109
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-09-07 18:53:00 -0300

    Intermediate JavaScript - 2. Async JS: Trivia Night exercise

---
+++ Intermediate-JavaScript/Async JS/Trivia-Night/trivia-night.js
@@ -0,0 +1,45 @@
+const quizContainer = document.getElementById("quiz");
+const startButton = document.getElementById("startBtn");
+
+async function fetchTriviaQuestions() {
+  const response = await fetch("https://opentdb.com/api.php?amount=10");
+  
+  if (response.ok === false) {
+    throw new Error("Failed to get trivia questions");
+  }
+
+  const data = await response.json();
+  return data.results;
+}
+
+function displayTriviaQuestions(questions) {
+  questions.forEach((question, index) => {
+    const questionElement = document.createElement("div");
+    questionElement.classList.add("question");
+    questionElement.innerHTML = `<p>${index + 1}. ${question.question}</p>`;
+
+    const radioGroupName = `question${index}`;
+
+    const answers = [...question.incorrect_answers, question.correct_answer];
+
+    answers.forEach((answer) => {
+      const answerElement = document.createElement("div");
+      answerElement.classList.add("answer");
+      answerElement.innerHTML = `
+        <label>
+          <input id="question-${index}" type="radio" name="${radioGroupName}" value="${answer}">
+          ${answer}
+        </label>
+      `;
+      questionElement.appendChild(answerElement);
+    });
+
+    quizContainer.appendChild(questionElement); 
+  });
+}
+
+startButton.addEventListener("click", async () => {
+  startButton.disabled = true;
+  const questions = await fetchTriviaQuestions();
+  displayTriviaQuestions(questions);
+});
