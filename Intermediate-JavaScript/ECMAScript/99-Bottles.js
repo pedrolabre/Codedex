80a545694c7e271437c995d01edd9daf893c2f37
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-09-07 15:47:00 -0300

    Intermediate JavaScript - 1. ECMAScript: 99 Bottles exercise

---
+++ Intermediate-JavaScript/ECMAScript/99-Bottles.js
@@ -0,0 +1,17 @@
+// Change these to string template literals 💖
+
+//for (let i = 99; i > 0; i--) {
+//  console.log(i + " bottles of pop on the wall");
+//  console.log(i + " bottles of pop");
+//  console.log("take one down, pass it around");
+//  console.log(i - 1 + " bottles of pop on the wall");
+//  console.log();
+//}
+
+for (let i = 99; i > 0; i--) {
+  console.log(`${i} bottles of pop on the wall`);
+  console.log(`${i} bottles of pop`);
+  console.log("take one down, pass it around");
+  console.log(`${i - 1} bottles of pop on the wall`);
+  console.log();
+}
