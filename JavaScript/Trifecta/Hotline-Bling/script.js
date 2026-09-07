031eb77f8bbd8d94d5a2229b55bb24e8f054737c
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-09-07 13:30:00 -0300

    JavaScript - 8. The Trifecta: Hotline Bling exercise

---
+++ JavaScript/Trifecta/Hotline-Bling/script.js
@@ -0,0 +1,8 @@
+let drakePicTwo = document.getElementById("drake-pic-2");
+let heading = document.getElementById("heading-2");
+let headingOne = document.getElementById("heading-1");
+
+drakePicTwo.src = "https://i.imgur.com/RGbh6zY.png";
+
+headingOne.innerText = "Watching hours of React Native tutorials";
+heading.innerText = "Deploying the backend on Render and Supabase";
