7f9212139f13105c3bd82e9c4600959ff6fcc956
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-09-07 10:52:00 -0300

    JavaScript - 7. Objects: Pokemon exercise

---
+++ JavaScript/Objects/Pokemon.js
@@ -0,0 +1,14 @@
+const pokemon = {
+  name: "Pikachu",
+  type: "Electric ⚡️",
+  level: 25
+};
+
+pokemon.isCaught = false;
+
+console.log(pokemon);
+
+pokemon.isCaught = true;
+pokemon.name = "Poku";
+
+console.log(pokemon);
